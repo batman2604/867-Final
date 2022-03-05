419e7f14eba8509e2e7ee023f6ef2385b4c59c96
Ruth Berkun <67928066+happyRainDrop@users.noreply.github.com>
2022-03-05 11:30:43 -0800

    Add limelight constants

--- Final/src/main/include/drive.hpp
+++ Final/src/main/include/drive.hpp
@@ -1,8 +1,17 @@
 #ifndef DRIVEH
     #define DRIVEH
+        
+        // Limelight constants
+        #define LIMELIGHT_ANGLE = 30; // in degrees
+        #define TARGET_HEIGHT = 104; // inches 
+        #define LIMELIGHT_HEIGHT = 12; // inches
+        #define PI 3.14159265;
 
+        // Encoder constants
         #define ENCROT 4096 //Full rotation in Encoder Units
+
         extern  frc::Joystick control;
+        double getTargetHorizontalDistance(); // Limelight-measured distance
         void dSetup();
         void autoMain(int target);
         void turn(int degree);
